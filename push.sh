f92cdcc2df2f7a2cb3bf4da5f9cc217d059f064e
HD Moore <x@hdm.io>
2015-11-24 07:49:13 -0600

    Updates

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git add --all && git commit . -m 'Updates' && git push origin master
-jekyll build && cd _site && git add --all && git commit . -m 'Updates' && git push origin gh-pages
+jekyll build && cd _site && git add --all && git commit . -m 'Updates' && git push origin gh-pages && rsync -arv --exclude=.git . root@hdm.io:/var/www/wardial/
